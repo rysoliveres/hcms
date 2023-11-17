8b09aba96abda993fe18542aa3e1bda83a21c992
rysoliveres <Ryan.Soliveres@ibm.com>
2023-11-18 07:31:06 +0800

    Routing

--- src/utilities/navigation.js
+++ src/utilities/navigation.js
@@ -7,18 +7,10 @@ import PageDetails from '../pages/PageDetails';
 const Navigation = () => {
     return (
         <HashRouter basename="/">
-            <div>
-                {/* <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/watsonx">About</Link></li>
-                </ul>
-                <hr /> */}
                 <Routes>
                     <Route exact path="/" element={<HomePage />} />
                     <Route path="/page/:id" element={<PageDetails />} />
-                    {/* <Route path="/watsonx" element={<WatsonxPage />} /> */}
                 </Routes>
-            </div>
         </HashRouter>
     )
 }
