b381560f6b2401a3ff4a992e37d4c4b33141791e
rysoliveres <Ryan.Soliveres@ibm.com>
2023-11-18 06:37:48 +0800

    Routing

--- src/utilities/navigation.js
+++ src/utilities/navigation.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'; //Use Routes instead of Switch
+import { BrowserRouter, Route, Routes, useParams, HashRouter } from 'react-router-dom'; //Use Routes instead of Switch
 import { React } from 'react'
 import HomePage from '../pages/Home';
 import WatsonxPage from '../pages/watsonx-page';
@@ -6,13 +6,13 @@ import PageDetails from '../pages/PageDetails';
 
 const Navigation = () => {
     return (
-        <BrowserRouter>
-            <Routes>
+        <HashRouter basename="/">
+            
                 <Route exact path="/" element={<HomePage />} />
                 <Route path="/page/:id" element={<PageDetails />} />
                 <Route path="/watsonx" element={<WatsonxPage />} />
-            </Routes>
-        </BrowserRouter>
+           
+        </HashRouter>
     )
 }
 
